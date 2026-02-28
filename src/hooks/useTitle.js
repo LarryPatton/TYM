a4059623db01344912db48e528fca203d599a95a
unknown <adiya@game.ntes>
2026-02-28 21:19:11 +0800

    更新: 周六 2026/02/28 21:19:11

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 export const useTitle = (title) => {
   useEffect(() => {
     const prevTitle = document.title;
-    document.title = `${title} | LOGO.`; // 后缀可以统一修改，例如 " | 提示词库"
+    document.title = title;
     
     // 页面卸载时恢复之前的标题（可选，通常不需要）
     return () => {
